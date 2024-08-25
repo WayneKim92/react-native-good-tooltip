e5c7e5a18edf47ba550b07a84da6eff8a4ac86f8
gim-eunchan <sumel001@binarycorp.com>
2024-08-25 19:16:59 +0900

    refactor: getAnchorPoint 추출

--- src/index.tsx
+++ src/index.tsx
@@ -12,6 +12,7 @@ import {
   Image,
 } from 'react-native';
 import { withAnchorPoint } from 'react-native-anchor-point';
+import { getAnchorPoint } from './functions';
 
 const SIDE_ARROW_INSET = 12;
 const CLOSE_ICON_SIZE = 16;
@@ -248,61 +249,7 @@ export const Tooltip = ({
   );
 
   const transformsStyle = (() => {
-    let x = 0;
-    let y = 0;
-
-    // vertical
-    if (placement === 'top' && anchor === 'left') {
-      x = 0;
-      y = 1;
-    }
-    if (placement === 'top' && anchor === 'right') {
-      x = 1;
-      y = 1;
-    }
-    if (placement === 'top' && anchor === 'center') {
-      x = 0.5;
-      y = 1;
-    }
-
-    if (placement === 'bottom' && anchor === 'left') {
-      x = 0;
-      y = 0;
-    }
-    if (placement === 'bottom' && anchor === 'center') {
-      x = 0.5;
-      y = 0;
-    }
-    if (placement === 'bottom' && anchor === 'right') {
-      x = 1;
-      y = 0;
-    }
-
-    // horizontal
-    if (placement === 'left' && anchor === 'top') {
-      x = 1;
-      y = 0;
-    }
-    if (placement === 'left' && anchor === 'center') {
-      x = 1;
-      y = 0.5;
-    }
-    if (placement === 'left' && anchor === 'bottom') {
-      x = 1;
-      y = 1;
-    }
-    if (placement === 'right' && anchor === 'top') {
-      x = 0;
-      y = 0;
-    }
-    if (placement === 'right' && anchor === 'center') {
-      x = 0;
-      y = 0.5;
-    }
-    if (placement === 'right' && anchor === 'bottom') {
-      x = 0;
-      y = 1;
-    }
+    const { x, y } = getAnchorPoint(placement, anchor);
 
     if (currentIsVisible) {
       Animated.spring(animatedValue, {
